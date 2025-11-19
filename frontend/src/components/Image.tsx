f41dee148d841e04ddb9b843e0c7095badc0c2b4
JamesWebbTelescope <112393315+JamesWebbTelescope@users.noreply.github.com>
2025-11-19 14:43:13 +0100

    Update Image.tsx

--- frontend/src/components/Image.tsx
+++ frontend/src/components/Image.tsx
@@ -1,11 +1,11 @@
-import car from '../assets/raspberry-pi-5.png'; // Import the image
+import raspberry from '../assets/raspberry-pi-5.png'; // Import the image
 
 function RaspberryPiImage() {
 return (
-<div className='justify-center'>
-<img src={car} alt="The Raspberry Pi" /> {/* Use the image */}
-</div>
-);
+    <div className='justify-center'>
+    <img src={raspberry} alt="The Raspberry Pi" /> {/* Use the image */}
+    </div>
+    );
 }
 
 export default RaspberryPiImage;
